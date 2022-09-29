b19901fdf55861f678bbf8788c15a439f0a866c0
Alvaro <aguerrar06@gmail.com>
2022-09-29 11:15:17 +0200

    Primer microservicio

---
+++ springboot-service-productos/src/test/java/com/microservicios/productos/SpringbootServiceProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

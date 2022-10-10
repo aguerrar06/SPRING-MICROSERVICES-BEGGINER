19f025be5bffdeb62d5429a10e3a6cee1ff27e12
Álvaro <aguerrar06@gmail.com>
2022-10-10 16:47:00 +0200

    Balanceo de carga en RestTemplate usando Ribbon

--- springboot-service-productos/src/main/java/com/microservicios/productos/controller/ProductoController.java
+++ springboot-service-productos/src/main/java/com/microservicios/productos/controller/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,10 @@ import com.microservicios.productos.service.ProductoService;
 public class ProductoController {
 
 	@Autowired
-	private Environment env;
+	private Environment env; // Extraer cualquier dato del application.properties
+	
+	@Value("${server.port}") // Extraer el puerto con la anotación Value
+	private Integer port;
 	
 	@Autowired
 	private ProductoService productoService;
@@ -24,7 +28,8 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto> getAllProductos() {
 		return this.productoService.findAll().stream().map(producto -> {
-			producto.setPuerto(Integer.parseInt(this.env.getProperty("local.server.port")));
+			//producto.setPuerto(Integer.parseInt(this.env.getProperty("local.server.port")));
+			producto.setPuerto(this.port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
@@ -32,8 +37,8 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public Producto getAllProductos(@PathVariable Long id) {
 		Producto producto = this.productoService.findById(id);
-		producto.setPuerto(Integer.parseInt(this.env.getProperty("local.server.port")));
-		
+		//producto.setPuerto(Integer.parseInt(this.env.getProperty("local.server.port")));
+		producto.setPuerto(this.port);		
 		return producto;
 	}
 	
